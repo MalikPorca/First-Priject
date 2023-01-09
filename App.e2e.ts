8cf3b96e88c29e2a0e211ad88ffdb43fcac84f7c
Malik Porca <malik.porca.22@size.ba>
2023-01-09 19:36:43 +0100

    napravljena nova grana na koju se commitalo

---
+++ App.e2e.ts
@@ -0,0 +1,7 @@
+// na klik na polje input, dobiva se tastatura za unos
+//--------------------------------------------------------------------------------
+// nakon unesenog texta klik na button ispisuje task crne podloge sa unesenim textom
+// kao i button crbene boje i znaka X na istom tasku te on brise text u inputu
+// -------------------------------------------------------------------------------
+// klik na button sa znakom X brise prvi task u listi
+// -------------------------------------------------------------------------------
--- App.tsx
+++ App.tsx
@@ -1,47 +1,104 @@
-import React from "react";
-import { SafeAreaView, StyleSheet, TextInput, Button } from "react-native";
 
-const UselessTextInput = () => {
-  const [text, onChangeText] = React.useState(null);
-  const [number, onChangeNumber] = React.useState(null);
+import React, { Component } from 'react';
+import { SafeAreaView, StyleSheet, Button, TextInput, Text, View, Pressable, SectionList,  } from "react-native";
+import { Colors } from "react-native/Libraries/NewAppScreen";
+import { setTextRange } from 'typescript';
+import CheckBox from '@react-native-community/checkbox';
 
-  const SomeButton = () => {
-    alert("Ispisemo neki text po izboru ili pozovemo nesto drugo ovim buttonom, kao npr drugi page")
-  }
+const Input = () => {
+  const [text, onChangeText] = React.useState("");
+   const [text2, onChangeText2] = React.useState([]);
 
+   function SubmmitANDClear  (){
+     text2.push(text)
+    onChangeText("")
+    }
 
+    function clearAll  (){
+      text2.splice(text, 60)
+      onChangeText(text + " ")
+     }
+    
+
+    const del=(key)=>{
+           text2.splice(text2[key], 1)
+          onChangeText(text + " ")
+      }
+    
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{justifyContent:'space-between',flexDirection:'column',}}>
       <TextInput
         style={styles.input}
-        onChangeText={onChangeText}
-        value={text}
-        placeholder="input texst"
-      />
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeNumber}
-        value={number}
-        placeholder="input some number"
-        keyboardType="numeric"
+         defaultValue={text}
+        onChange={(e) => onChangeText(e.nativeEvent.text)}
+      />      
+       <Button
+       title="Submmit"
+       color={"#f194ff"}
+       onPress ={SubmmitANDClear}
+       /> 
         
-      />
-      <Button
-        title="Press me"
-        color="#f194ff"
-        onPress={SomeButton}
-      />
-    </SafeAreaView>
+       {text2.map((task, i)=>(
+         <Pressable style={styles.button}  >
+          <CheckBox
+              value={state.vue}
+              onValueChange={value =>
+                setState({
+                  ...state,
+                  vue: value,
+                })
+              }
+            />
+         <Text
+         style={{backgroundColor: 'black',color: "white", borderColor: "black", borderWidth: 1,  marginTop: 25, width: 320, height: 50,  textAlign: "left" , paddingLeft:10, justifyContent:'space-between', flexDirection:'row',}}
+         key={i}
+         >
+        {task+"             "}
+       
+
+        <Button 
+        key={i}
+        title='X'
+        color={"red"} 
+        onPress={del}
+        ></Button>
+        </Text> 
+        
+        </Pressable>
+        
+      ))}
+      
+        
+ <Button
+        title='Clear'
+        onPress={clearAll}
+        ></Button>
+    </SafeAreaView>   
+    
   );
 };
 
 const styles = StyleSheet.create({
   input: {
-    height: 40,
+    height: 50,
     margin: 12,
+    marginTop: 65,
     borderWidth: 1,
-    padding: 5,
+    padding: 10,
+    borderRadius:20
   },
+    button: {
+      alignItems: 'center',
+      justifyContent: 'flex-end',  
+      position:'relative'
+      
+     
+      
+      
+    },
 });
 
-export default UselessTextInput;
+
+
+export default Input; 
+
