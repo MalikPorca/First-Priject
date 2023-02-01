a5bd1eebae731458cfe626266ab20a5d304dcbc2
Malik Porca <malik.porca.22@size.ba>
2023-02-01 14:26:11 +0100

    skracivanje

--- App.tsx
+++ App.tsx
@@ -1,8 +1,6 @@
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { SideMenu } from './Drawer';
-import { BottomTab } from './BottomTab';
-import { View } from 'react-native';
 export default function App(){
   return(
     
